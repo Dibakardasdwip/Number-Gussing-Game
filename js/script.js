f8b559fac568ac7f395915755975633e793b7668
Dibakardasdwip <121dibakar@gmail.com>
2023-11-26 02:48:13 +0600

    complete

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ class UI {
     this.form = document.querySelector("form");
     this.userGuess = document.querySelector("#userInput");
     this.correctAns = Math.ceil(Math.random() * 10);
+    this.count = 3;
     console.log(this.correctAns);
 
     // console.log(this.userGuess.value);
@@ -18,50 +19,56 @@ class UI {
     this.mainPrompt.classList.replace("text-success", "text-primary");
     this.mainPrompt.style.fontSize = "28px";
   }
-  // randomNum() {
-  //   let ranNum = Math.ceil(Math.random() * 10);
-  //   return ranNum;
-  // }
+
   checkingAnswer(e) {
     e.preventDefault();
+    console.log(e);
+
     let userText = parseInt(document.querySelector("#userInput").value);
     let re = /^[0-9]?[0-9]$/;
     let numberCheck = re.test(userText);
-    if (numberCheck && userText <= 10 && userText > 0) {
-      // AnswerChecking
-      if (userText !== this.correctAns) {
-        this.mainPrompt.textContent = "";
 
-        if (userText < this.correctAns) {
-          this.guessPrompt[0].removeAttribute("style");
-          this.mainPrompt.textContent = "";
-          this.guessPrompt[1].setAttribute("style", "visibility:hidden");
-          console.log(this.guessPrompt[0].innerHTML);
-        } else {
-          this.guessPrompt[1].removeAttribute("style");
-          this.mainPrompt.textContent = "";
-          this.guessPrompt[0].setAttribute("style", "visibility:hidden");
-        }
-      } else if (userText === this.correctAns) {
-        this.mainPrompt.textContent = "You Win!";
-        this.mainPrompt.style.fontSize = "50px";
-        this.guessPrompt[0].setAttribute("style", "visibility:hidden");
+    this.count -= 1;
+    this.chance.innerHTML = `Chance ${this.count}`;
+
+    if (this.count <= 0 && userText != this.correctAns) {
+      this.mainPrompt.textContent = "You Lose";
+      this.userGuess.disabled = true;
+      document.querySelector("#guess").disabled = true;
+      return;
+    }
+
+    if (userText !== this.correctAns) {
+      if (userText < this.correctAns) {
+        this.guessPrompt[0].removeAttribute("style");
+
         this.guessPrompt[1].setAttribute("style", "visibility:hidden");
+        console.log(this.guessPrompt[0].innerHTML);
+        this.chance.innerHTML = `Chance: ${this.count}`;
+      } else {
+        this.guessPrompt[1].removeAttribute("style");
+        this.guessPrompt[0].setAttribute("style", "visibility:hidden");
       }
-
-      // console.log(numberCheck);
-    } else {
-      this.mainPrompt.textContent = "Guess a number from 1  to 10";
-      this.mainPrompt.classList.replace("text-primary", "text-danger");
+      this.mainPrompt.textContent = "";
+      this.mainPrompt.textContent = `Wrong ${3 - this.count}X`;
+      this.userGuess.value = "";
+    } else if (userText === this.correctAns) {
+      this.mainPrompt.textContent = "You Win!";
+      this.mainPrompt.style.fontSize = "50px";
       this.guessPrompt[0].setAttribute("style", "visibility:hidden");
       this.guessPrompt[1].setAttribute("style", "visibility:hidden");
+      this.userGuess.disabled = true;
+      document.querySelector("#guess").disabled = true;
     }
+
+    console.log(this.count);
   }
 }
 
 let ui = new UI();
 ui.starUp();
-// let a = ui.randomNum();
-// console.log(a);
 
 ui.form.addEventListener("submit", ui.checkingAnswer.bind(ui));
+document
+  .querySelector("#reset")
+  .addEventListener("click", (e) => window.location.reload());
